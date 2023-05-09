a1b78a330b3cdb99fd032f7f119c3cd2a285c8b3
Ivan <39631093+IvSiln@users.noreply.github.com>
2023-05-10 03:47:45 +0500

    Update FilmStorage.java
    reload

--- src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
@@ -1,18 +1,37 @@
 package ru.yandex.practicum.filmorate.storage;
 
 import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.model.Genre;
+import ru.yandex.practicum.filmorate.model.Mpa;
 
-import java.sql.SQLException;
 import java.util.List;
 import java.util.Optional;
 
 public interface FilmStorage {
 
-    Film createFilm(Film film) throws SQLException;
+    Film createFilm(Film film);
 
-    Film updateFilm(Film film) throws SQLException;
+    Film updateFilm(Film film);
 
     Optional<Film> deleteFilm(long id);
 
     List<Film> getAllFilms();
+
+    List<Film> getTopFilms(Integer count);
+
+    Optional<Film> getFilmById(long filmId);
+
+    List<Genre> getAllGenres();
+
+    List<Mpa> getAllMpa();
+
+    Mpa getMpaById(long id);
+
+    Genre getGenreById(long id);
+
+    boolean isFilmExist(Long filmId);
+
+    void addLike(Long filmId, Long userId);
+
+    void deleteLike(Long filmId, Long userId);
 }
