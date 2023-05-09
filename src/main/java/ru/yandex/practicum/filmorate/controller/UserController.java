d59beec70b2c933583dbd4067e7e2692331d1363
Ivan <39631093+IvSiln@users.noreply.github.com>
2023-05-10 03:41:30 +0500

    Update UserController.java
    reload

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -5,21 +5,22 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import ru.yandex.practicum.filmorate.exception.*;
+import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.model.User;
-import ru.yandex.practicum.filmorate.service.UserDbService;
+import ru.yandex.practicum.filmorate.service.UserService;
 
 import javax.validation.Valid;
-import java.util.*;
+import javax.validation.constraints.Positive;
+import java.util.List;
 
 @RestController
 @Slf4j
 @RequestMapping("/users")
 public class UserController {
-    private final UserDbService userService;
+    private final UserService userService;
 
     @Autowired
-    public UserController(UserDbService userService) {
+    public UserController(UserService userService) {
         this.userService = userService;
     }
 
@@ -29,51 +30,51 @@ public class UserController {
     }
 
     @GetMapping("/{id}")
-    public User getUser(@PathVariable Long id) {
-        if (id <= 0) {
-            throw new ValidationException("id должен быть больше 0");
-        }
-        return userService.getUserById(id);
+    public ResponseEntity<User> getUser(@PathVariable @Positive(message = "id должен быть больше 0") Long id) {
+        User user = userService.getUserById(id);
+        return ResponseEntity.ok(user);
     }
 
     @PostMapping
-    public User addUser(@Valid @RequestBody User user) {
+    public ResponseEntity<User> addUser(@Valid @RequestBody User user) {
         User createdUser = userService.createUser(user);
         log.trace("Добавлен пользователь: " + createdUser);
-        return createdUser;
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdUser);
     }
 
     @PutMapping
-    public User updateUser(@Valid @RequestBody User updateUser) {
+    public ResponseEntity<User> updateUser(@Valid @RequestBody User updateUser) {
         User updatedUser = userService.updateUser(updateUser);
         log.trace("Изменен пользователь: " + updatedUser);
-        return updateUser;
+        return ResponseEntity.ok(updatedUser);
     }
 
     @PutMapping("/{id}/friends/{friendId}")
-    public ResponseEntity addFriend(@PathVariable Long id, @PathVariable Long friendId) {
+    public ResponseEntity<Void> addFriend(@PathVariable Long id, @PathVariable Long friendId) {
         userService.addFriend(id, friendId);
         log.trace(String.format("Пользователь с ID: %d добавлен в друзья пользователя с ID: %d", friendId, id));
-        return ResponseEntity.status(HttpStatus.OK).build();
+        return ResponseEntity.ok().build();
     }
 
     @DeleteMapping("/{id}/friends/{friendId}")
-    public ResponseEntity deleteFriend(@PathVariable Long id, @PathVariable Long friendId) {
+    public ResponseEntity<Void> deleteFriend(@PathVariable Long id, @PathVariable Long friendId) {
         userService.deleteFriend(id, friendId);
         log.trace(String.format("Пользователь с ID: %d удален из друзей пользователя с ID: %d", friendId, id));
-        return ResponseEntity.status(HttpStatus.OK).build();
+        return ResponseEntity.ok().build();
     }
 
     @GetMapping("/{id}/friends")
-    public List<User> getAllFriends(@PathVariable Long id) {
+    public ResponseEntity<List<User>> getAllFriends(@PathVariable Long id) {
         if (userService.getUserById(id) == null) {
             throw new NotFoundException("Пользователь с ID " + id + " не существует.");
         }
-        return userService.getAllFriends(id);
+        List<User> friends = userService.getAllFriends(id);
+        return ResponseEntity.ok(friends);
     }
 
     @GetMapping("/{id}/friends/common/{otherId}")
-    public List<User> getCommonFriends(@PathVariable Long id, @PathVariable Long otherId) {
-        return userService.getCommonFriends(id, otherId);
+    public ResponseEntity<List<User>> getCommonFriends(@PathVariable Long id, @PathVariable Long otherId) {
+        List<User> commonFriends = userService.getCommonFriends(id, otherId);
+        return ResponseEntity.ok(commonFriends);
     }
 }
