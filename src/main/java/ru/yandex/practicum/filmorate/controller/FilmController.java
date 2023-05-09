0247828035b66ae7212fa2301957aa9bada70ef5
Ivan <39631093+IvSiln@users.noreply.github.com>
2023-05-10 03:40:40 +0500

    Update FilmController.java
    reload

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -6,79 +6,77 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.model.Film;
-import ru.yandex.practicum.filmorate.service.FilmDbService;
+import ru.yandex.practicum.filmorate.service.FilmService;
 
 import javax.validation.Valid;
-import java.sql.SQLException;
+import javax.validation.constraints.Positive;
 import java.util.*;
 
 @RestController
 @Slf4j
 @RequestMapping("/films")
 public class FilmController {
-    private final FilmDbService filmService;
+    private final FilmService filmService;
 
     @Autowired
-    public FilmController(FilmDbService filmService) {
+    public FilmController(FilmService filmService) {
         this.filmService = filmService;
     }
 
     @GetMapping
-    public List<Film> getFilms() {
-        return filmService.getAllFilms();
+    public ResponseEntity<List<Film>> getFilms() {
+        List<Film> films = filmService.getAllFilms();
+        return ResponseEntity.ok(films);
     }
 
     @GetMapping("/{id}")
-    public Film getFilm(@PathVariable long id) {
-        Film film = filmService.getFilmById(id);
-        log.trace("Запрошен фильм: " + film);
-        return film;
+    public ResponseEntity<Film> getFilm(@PathVariable long id) {
+        Optional<Film> film = filmService.getFilmById(id);
+        log.trace("Requested film: {}", film);
+        return film.map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
     }
 
     @PostMapping
-    public Film addFilm(@Valid @RequestBody Film newFilm) throws SQLException {
+    public ResponseEntity<Film> addFilm(@Valid @RequestBody Film newFilm) {
         Film createdFilm = filmService.createFilm(newFilm);
         log.trace("Добавлен фильм: " + createdFilm);
-        return createdFilm;
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdFilm);
     }
 
     @PutMapping
-    public Film updateFilm(@Valid @RequestBody Film updateFilm) throws SQLException {
+    public ResponseEntity<Film> updateFilm(@Valid @RequestBody Film updateFilm) {
         Film updatedFilm = filmService.updateFilm(updateFilm);
         log.trace("Обновлен фильм: " + updatedFilm);
-        return updatedFilm;
+        return ResponseEntity.ok(updatedFilm);
     }
 
     @DeleteMapping("/{id}")
-    public Film deleteFilm(@PathVariable long id) {
-        Film deletedFilm = filmService.deleteFilm(id);
-        log.trace("Удален фильм: {}", deletedFilm);
-        return deletedFilm;
+    public ResponseEntity<Film> deleteFilm(@PathVariable long id) {
+        Optional<Film> deletedFilm = filmService.deleteFilm(id);
+        log.trace("Deleted film: {}", deletedFilm);
+        return deletedFilm.map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
     }
 
     @PutMapping("/{id}/like/{userId}")
-    public ResponseEntity addLike(@PathVariable Long id,
-                                  @PathVariable Long userId) {
+    public ResponseEntity<Void> addLike(@PathVariable Long id, @PathVariable Long userId) {
         filmService.addLike(id, userId);
         log.trace(String.format("Пользователь с ID: %d отметил лайком фильм с ID: %d", userId, id));
         return ResponseEntity.status(HttpStatus.OK).build();
     }
 
     @DeleteMapping("/{id}/like/{userId}")
-    public ResponseEntity deleteLike(@PathVariable Long id,
-                                     @PathVariable Long userId) {
+    public ResponseEntity<Void> deleteLike(@PathVariable Long id, @PathVariable Long userId) {
         filmService.deleteLike(id, userId);
         log.trace(String.format("Пользователь с ID: %d удалил лайк фильма с ID: %d ", userId, id));
         return ResponseEntity.status(HttpStatus.OK).build();
     }
 
     @GetMapping("/popular")
-    public List<Film> getTopFilms(
-            @RequestParam(value = "count", defaultValue = "10", required = false) Integer count) {
+    public ResponseEntity<List<Film>> getTopFilms(@RequestParam(value = "count", defaultValue = "10", required = false) @Positive(message = "count должен быть больше 0") Integer count) {
         if (count <= 0) {
             throw new IllegalArgumentException("count должен быть целым числом больше 0, получено " + count);
         }
         List<Film> topFilms = filmService.getTopFilms(count);
-        return topFilms;
+        return ResponseEntity.ok(topFilms);
     }
 }
