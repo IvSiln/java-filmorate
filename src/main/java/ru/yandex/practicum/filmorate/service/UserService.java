80c9c282e08b5ed246a04ddc9a5e2217929b3859
Ivan <39631093+IvSiln@users.noreply.github.com>
2023-05-10 03:46:01 +0500

    Update UserService.java
    reload

--- src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
@@ -1,33 +1,34 @@
 package ru.yandex.practicum.filmorate.service;
 
 import org.apache.commons.lang3.StringUtils;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
-import ru.yandex.practicum.filmorate.exception.*;
+import ru.yandex.practicum.filmorate.exception.NotFoundException;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.User;
-import ru.yandex.practicum.filmorate.storage.InMemoryUserStorage;
+import ru.yandex.practicum.filmorate.storage.UserStorage;
 
 import java.util.List;
 import java.util.Optional;
 
 @Service
 public class UserService {
-    private final InMemoryUserStorage inMemoryUserStorage;
-    private long counterId;
+    private final UserStorage userStorage;
 
-    @Autowired
-    public UserService(InMemoryUserStorage inMemoryUserStorage) {
-        this.inMemoryUserStorage = inMemoryUserStorage;
-        counterId = 0;
+    public UserService(@Qualifier("userDbStorage") UserStorage userStorage) {
+        this.userStorage = userStorage;
     }
 
-    public long getCounterId() {
-        return counterId;
+    public User getUserById(long id) {
+        if (userStorage.getUserById(id) != null) {
+            return userStorage.getUserById(id);
+        } else {
+            throw new NotFoundException("Не найден пользователь с ID: " + id);
+        }
     }
 
-    private long incrementCounterId() {
-        counterId++;
-        return counterId;
+    public Optional<User> getUserByEmail(String email) {
+        return userStorage.getUserByEmail(email);
     }
 
     public User createUser(User user) {
@@ -37,64 +38,39 @@ public class UserService {
         if (StringUtils.isBlank(user.getName())) {
             user.setName(user.getLogin());
         }
-        user.setId(incrementCounterId());
-        inMemoryUserStorage.createUser(user);
-        return user;
+        return userStorage.createUser(user);
+
     }
 
     public User updateUser(User user) {
-        getUserById(user.getId());
-
-        getUserByEmail(user.getEmail()).ifPresent(existingUser -> {
-            if (existingUser.getId() != user.getId()) {
-                throw new ValidationException(String.format("Пользователю нельзя назначить такой email: %s, он уже используется.", user.getEmail()));
-            }
-        });
-
+        if (getUserById(user.getId()) == null) {
+            throw new NotFoundException("Нет пользователя с ID " + user.getId());
+        }
+        if (getUserByEmail(user.getEmail()).isPresent() && (getUserByEmail(user.getEmail()).get().getId() != user.getId())) {
+            throw new ValidationException(String.format("Пользователю нельзя назначить такой email: %s, он уже используется.", user.getEmail()));
+        }
         if (StringUtils.isBlank(user.getName())) {
             user.setName(user.getLogin());
         }
-        User updatedUser = inMemoryUserStorage.updateUser(user);
+        User updatedUser = userStorage.updateUser(user);
         return updatedUser;
     }
 
-    public User getUserById(long id) {
-        if (inMemoryUserStorage.getUserById(id) != null) {
-            return inMemoryUserStorage.getUserById(id);
-        } else {
-            throw new NotFoundException("Не найден пользователь с ID: " + id);
-        }
-    }
-
-    public Optional<User> getUserByEmail(String email) {
-        return inMemoryUserStorage.getUserByEmail(email);
-    }
-
     public List<User> getAllUsers() {
-        return inMemoryUserStorage.getAllUsers();
+        return userStorage.getAllUsers();
     }
 
-    public void addFriend(long userId, long friendId) {
-        User user1 = getUserById(userId);
-        User user2 = getUserById(friendId);
-        if (user1 == null) {
-            throw new NotFoundException("Пользователь с ID " + userId + " не существует.");
-        }
-        if (user2 == null) {
-            throw new ValidationException("Пользователь с ID " + friendId + " не существует.");
-        }
+    public boolean addFriend(long userId, long friendId) {
+        validateUsersExistence(userId, friendId);
 
-        if (!user1.addFriend(friendId)) {
+        if (userStorage.isFriend(userId, friendId)) {
             throw new ValidationException(String.format("Пользователь с ID: %d уже является другом пользователю с ID: %d", friendId, userId));
         }
-        if (!user2.addFriend(userId)) {
-            throw new ValidationException(String.format("Пользователь с ID: %d уже является другом пользователю с ID: %d", userId, friendId));
-        }
-        inMemoryUserStorage.updateUser(user1);
-        inMemoryUserStorage.updateUser(user2);
+        userStorage.addFriend(userId, friendId);
+        return true;
     }
 
-    public void deleteFriend(long userId, long friendId) {
+    private void validateUsersExistence(long userId, long friendId) {
         User user1 = getUserById(userId);
         User user2 = getUserById(friendId);
         if (user1 == null) {
@@ -103,30 +79,36 @@ public class UserService {
         if (user2 == null) {
             throw new NotFoundException("Пользователь с ID " + friendId + " не существует.");
         }
-        if (!user1.deleteFriend(friendId)) {
-            throw new NotFoundException(String.format("У пользователя ID: %d  нет друга с ID: %d", friendId, userId));
-        }
-        if (!user2.deleteFriend(userId)) {
-            throw new NotFoundException(String.format("У пользователя ID: %d  нет друга с ID: %d", userId, friendId));
-        }
-        inMemoryUserStorage.updateUser(user1);
-        inMemoryUserStorage.updateUser(user2);
     }
 
-    public List<User> getAllFriends(long userId) {
+    public boolean deleteFriend(long userId, long friendId) {
+        validateUsersExistence(userId, friendId);
+        if (!userStorage.isFriend(userId, friendId)) {
+            throw new ValidationException(String.format("У пользователя ID: %d  нет друга с ID: %d", userId, friendId));
+        }
+        userStorage.deleteFriend(userId, friendId);
+        return true;
+    }
+
+
+    public List<User> getAllFriends(Long userId) {
         if (getUserById(userId) == null) {
             throw new NotFoundException("Пользователь с ID " + userId + " не существует.");
         }
-        return inMemoryUserStorage.getAllFriends(userId);
+        return userStorage.getAllFriends(userId);
     }
 
-    public List<User> getCommonFriends(long id, long otherId) {
+    public List<User> getCommonFriends(Long id, Long otherId) {
         if (getUserById(id) == null) {
             throw new NotFoundException("Пользователь с ID " + id + " не существует.");
         }
         if (getUserById(otherId) == null) {
             throw new NotFoundException("Пользователь с ID " + otherId + " не существует.");
         }
-        return inMemoryUserStorage.getCommonFriends(id, otherId);
+        return userStorage.getCommonFriends(id, otherId);
+    }
+
+    public boolean isUserExist(Long userId) {
+        return userStorage.isUserExist(userId);
     }
 }
